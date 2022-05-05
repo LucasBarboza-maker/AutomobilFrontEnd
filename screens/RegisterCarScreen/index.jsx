83d81a1ae97c091c957863f38001692c571f510d
LucasBarboza-maker <Lucasrb18@gmail.com>
2022-05-05 18:46:22 -0300

    Adding dropdown
    Adding dropdown to choose gas or alcohol or your vehicle type

--- screens/RegisterCarScreen/index.jsx
+++ screens/RegisterCarScreen/index.jsx
@@ -1,6 +1,7 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { useEffect, useRef, useState } from 'react';
 import { View, Button, Text, Dimensions, FlatList, TextInput, Animated, Pressable } from 'react-native';
+import Constants from 'expo-constants';
 
 import defaultStyles from '../../global/styles/styles'
 
@@ -8,14 +9,16 @@ import MaskInput from 'react-native-mask-input';
 
 import CarCard from '../../components/cards/searchCarCard.jsx';
 
+import { Picker } from '@react-native-picker/picker';
+
 const styles = {
     container: {
         flex: 1,
         backgroundColor: '#ececec',
         alignItems: 'center',
-        minHeight: Dimensions.get('window').height,
         borderTopLeftRadius: 30,
         borderTopRightRadius: 30,
+        minHeight: 100
     },
     imageWrapper: {
         backgroundColor: 'red',
@@ -26,37 +29,75 @@ const styles = {
     },
     formContainer: {
         width: '100%',
-        display:'flex',
-        padding:10
+        display: 'flex',
+        padding: 10,
+        minHeight: 100
     },
-    inputStyle:{
-        fontSize:30
+    inputStyle: {
+        fontSize: 30
     }
 }
 
 export default function Screen() {
 
     const [phone, setPhone] = useState('');
+    const [selectedLanguage, setSelectedLanguage] = useState();
 
     return (
         <>
             <StatusBar style="dark" />
-            <View style={{ height: 40, width: '100%' }}></View>
+            <View style={{ height: Constants.statusBarHeight, width: '100%' }}></View>
             <View style={styles.container}>
                 <View style={styles.imageWrapper}></View>
+                <Text style={{padding:10, fontSize:14}}>*Vamos comparar as informações com a foto do seu CRLV</Text>
                 <View style={styles.formContainer}>
                     <TextInput
-                    style={[defaultStyles.inputText, {fontSize:20, padding:10, borderColor:'#aaaaaa', borderWidth:1}]}
-                     placeholder='Modelo do Carro'
+                        style={[defaultStyles.inputText, { fontSize: 20, padding: 10, borderColor: '#aaaaaa', borderWidth: 1 }]}
+                        placeholder='Modelo do Carro'
                     />
-                    <MaskInput
+                    <TextInput
+                        style={[defaultStyles.inputText, { fontSize: 20, marginTop:10, padding: 10, borderColor: '#aaaaaa', borderWidth: 1 }]}
+                        placeholder='Cor Predominante'
+                    />
+                    {/* <MaskInput
                         value={phone}
-                        style={[defaultStyles.inputText, {fontSize:20, padding:10, marginTop:10}]}
+                        style={[defaultStyles.inputText, { fontSize: 20, padding: 10, marginTop: 10 }]}
                         onChangeText={(masked, unmasked) => {
-                            setPhone(masked);}}
-                            mask={['(', /\d/, /\d/, ')', ' ', /\d/, /\d/, /\d/, /\d/, /\d/, '-', /\d/, /\d/, /\d/, /\d/]}
-                            />
+                            setPhone(masked);
+                        }}
+                        mask={['(', /\d/, /\d/, ')', ' ', /\d/, /\d/, /\d/, /\d/, /\d/, '-', /\d/, /\d/, /\d/, /\d/]}
+                    /> */}
+                    <View
+                        style={[defaultStyles.inputText, { fontSize: 20, marginTop: 10,justifyContent:'center' }]}
+                    >
+                        <Picker
+                            
+                            selectedValue={selectedLanguage}
+                            onValueChange={(itemValue, itemIndex) =>
+                                setSelectedLanguage(itemValue)
+                            }>
+                            <Picker.Item style={{ fontSize: 20 }} color="#8e8e8e" label="Espécie / Tipo" value="none" />
+                            <Picker.Item style={{ fontSize: 20 }} label="Carro" value="car" />
+                            <Picker.Item style={{ fontSize: 20 }} label="Motocicleta(Moto)" value="motocycle" />
+                            <Picker.Item style={{ fontSize: 20 }} label="Van" value="van" />
+                        </Picker>
 
+                    </View>
+
+                    <View
+                        style={[defaultStyles.inputText, { fontSize: 20, marginTop: 10,justifyContent:'center' }]}
+                        >
+                        <Picker
+                            selectedValue={selectedLanguage}
+                            onValueChange={(itemValue, itemIndex) =>
+                                setSelectedLanguage(itemValue)
+                            }>
+                            <Picker.Item style={{ fontSize: 20 }} color="#8e8e8e" label="Combustível" value="none" />
+                            <Picker.Item style={{ fontSize: 20 }} label="Alcool" value="alcohol" />
+                            <Picker.Item style={{ fontSize: 20 }} label="Gasolina" value="gas" />
+                            <Picker.Item style={{ fontSize: 20 }} label="Alcool e Gasolina" value="alcoholAndGas" />
+                        </Picker>
+                    </View>
                 </View>
             </View>
         </>
