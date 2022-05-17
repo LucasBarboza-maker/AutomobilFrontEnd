b584d1f1aac4ece8b3c42ddc36bbb10367dad3f9
LucasBarboza-maker <61573957+LucasBarboza-maker@users.noreply.github.com>
2022-05-17 16:06:39 -0300

    Fixing dependencies

--- screens/RegisterCarScreen/index.jsx
+++ screens/RegisterCarScreen/index.jsx
@@ -2,6 +2,7 @@ import { StatusBar } from 'expo-status-bar';
 import React, { useEffect, useRef, useState } from 'react';
 import { View, Button, Text, Dimensions, FlatList, TextInput, Animated, Pressable } from 'react-native';
 import Constants from 'expo-constants';
+import { FontAwesome } from '@expo/vector-icons';
 
 import defaultStyles from '../../global/styles/styles'
 
@@ -21,11 +22,17 @@ const styles = {
         minHeight: 100
     },
     imageWrapper: {
-        backgroundColor: 'red',
         width: '100%',
         height: 300,
         borderBottomLeftRadius: 30,
-        borderBottomRightRadius: 30
+        borderBottomRightRadius: 30,
+        display:'flex',
+        justifyContent:'center',
+        alignItems:'center',
+        borderBottomWidth:1,
+        borderLeftWidth:1,
+        borderRightWidth:1,
+        borderColor:'#aaaaaa'
     },
     formContainer: {
         width: '100%',
@@ -48,15 +55,20 @@ export default function Screen() {
             <StatusBar style="dark" />
             <View style={{ height: Constants.statusBarHeight, width: '100%' }}></View>
             <View style={styles.container}>
-                <View style={styles.imageWrapper}></View>
-                <Text style={{padding:10, fontSize:14}}>*Vamos comparar as informações com a foto do seu CRLV</Text>
+                <View style={styles.imageWrapper}>
+                    <View style={{width:120, height:100, alignItems:'center', justifyContent:'center', display:'flex'}}>
+                        <FontAwesome name="camera" size={100} color="#aaaaaa" />
+                    </View>
+                    <Text style={{ padding: 10, fontSize: 14 }}>Precisamos de pelo menos 3 fotos do seu veiculo</Text>
+                </View>
+                <Text style={{ padding: 10, fontSize: 14 }}>*Vamos comparar as informações com a foto do seu CRLV</Text>
                 <View style={styles.formContainer}>
                     <TextInput
                         style={[defaultStyles.inputText, { fontSize: 20, padding: 10, borderColor: '#aaaaaa', borderWidth: 1 }]}
                         placeholder='Modelo do Carro'
                     />
                     <TextInput
-                        style={[defaultStyles.inputText, { fontSize: 20, marginTop:10, padding: 10, borderColor: '#aaaaaa', borderWidth: 1 }]}
+                        style={[defaultStyles.inputText, { fontSize: 20, marginTop: 10, padding: 10, borderColor: '#aaaaaa', borderWidth: 1 }]}
                         placeholder='Cor Predominante'
                     />
                     {/* <MaskInput
@@ -68,10 +80,10 @@ export default function Screen() {
                         mask={['(', /\d/, /\d/, ')', ' ', /\d/, /\d/, /\d/, /\d/, /\d/, '-', /\d/, /\d/, /\d/, /\d/]}
                     /> */}
                     <View
-                        style={[defaultStyles.inputText, { fontSize: 20, marginTop: 10,justifyContent:'center' }]}
+                        style={[defaultStyles.inputText, { borderColor: '#aaaaaa', borderWidth: 1, fontSize: 20, marginTop: 10, justifyContent: 'center' }]}
                     >
                         <Picker
-                            
+
                             selectedValue={selectedLanguage}
                             onValueChange={(itemValue, itemIndex) =>
                                 setSelectedLanguage(itemValue)
@@ -85,8 +97,8 @@ export default function Screen() {
                     </View>
 
                     <View
-                        style={[defaultStyles.inputText, { fontSize: 20, marginTop: 10,justifyContent:'center' }]}
-                        >
+                        style={[defaultStyles.inputText, { borderColor: '#aaaaaa', borderWidth: 1, fontSize: 20, marginTop: 10, justifyContent: 'center' }]}
+                    >
                         <Picker
                             selectedValue={selectedLanguage}
                             onValueChange={(itemValue, itemIndex) =>
